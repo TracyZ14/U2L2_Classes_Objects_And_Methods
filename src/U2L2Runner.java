c75df67e49756ccfdf1f497cf8e4f73a798f7364
unknown <BT_4N2_07@BKTECH.LOCAL>
2023-09-22 14:09:12 -0400

    Initial commit

---
+++ src/U2L2Runner.java
@@ -0,0 +1,14 @@
+public class U2L2Runner {
+    public static void U2L2(String[] args) {
+        // Code segment I
+        // int len = 65;
+        // int wid = len + 10;
+        // Rectangle rect = new Rectangle(len, wid);
+
+        // Code segment II
+        // Rectangle rect = new Rectangle(10.0, 30.0);
+
+        // Code segment III
+        // Rectangle rect = new Rectangle(0);
+    }
+}
